c688e425c1b764bebac31da8c7813910c3f2fa3f
Tachibana446 <stinger33z@gmail.com>
2017-03-10 16:15:33 +0900

    欲しい機能はできた

--- popup.js
+++ popup.js
@@ -1,16 +1,55 @@
 $(function() {
-    $("#debug").click(function() {
-        console.log("click1");
-        var array = ["a", "b", "c"];
-        chrome.storage.sync.set({
-            "debug": array
-        }, function() {});
+    chrome.storage.sync.get("arr", function(value) {
+        if (Object.keys(value).length === 0) {
+            value = [];
+        } else {
+            value = value.arr;
+        }
+        console.log(value);
+        chrome.tabs.getSelected(function(tab) {
+            if (value.indexOf(tab.url) == -1) {
+                $("#removeButton").prop("disabled", true);
+            } else {
+                $("#addButton").prop("disabled", true);
+            }
+
+            $("#addButton").click(function() {
+                value.push(tab.url);
+                chrome.storage.sync.set({
+                    arr: value
+                });
+                $("#addButton").prop("disabled", true);
+                $("#removeButton").prop("disabled", false);
+            });
+            $("#removeButton").click(function() {
+                value = value.filter(function(v) {
+                    return v != tab.url;
+                });
+                chrome.storage.sync.set({
+                    arr: value
+                });
+                $("#addButton").prop("disabled", false);
+                $("#removeButton").prop("disabled", true);
+            });
+        });
     });
 
-    $("#debug2").click(function() {
-        console.log("click2");
-        chrome.storage.sync.get("debug", function(value) {
-            console.table(value);
+    $("#randomButton").click(function() {
+        chrome.storage.sync.get("arr", function(value) {
+            if (Object.keys(value).length !== 0) {
+                var index = Math.floor(Math.random() * (value.arr.length + 1));
+                $("#link").attr({
+                    href: value.arr[index]
+                });
+                $("#link").text(value.arr[index]);
+            }
         });
+    });
+
+    $("#clearButton").click(function() {
+        chrome.storage.sync.set({
+            arr: []
+        });
+        $("#addButton").prop("disabled", false);
     });
 });
